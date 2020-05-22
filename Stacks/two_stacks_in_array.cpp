e82a08dcce2ec17cbf135a48819c9ff2a17ee8cc
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2020-05-22 14:52:40 +0530

    two_stacks_one_array

---
+++ Stacks/two_stacks_in_array.cpp
@@ -0,0 +1,35 @@
+/* The method push to push element into the stack 2 */
+void twoStacks :: push1(int x)
+{
+    arr[++top1] = x;
+}
+   
+/* The method push to push element into the stack 2*/
+void twoStacks ::push2(int x)
+{
+    arr[--top2] = x;
+}
+   
+/* The method pop to pop element from the stack 1 */
+//Return the popped element
+int twoStacks ::pop1()
+{
+    if(top1==-1){
+        return(-1);
+    }
+    int d = arr[top1];
+    arr[top1--] = -1;
+    return(d);
+}
+
+/* The method pop to pop element from the stack 2 */
+//Return the popped element
+int twoStacks :: pop2()
+{
+    if(top2==size){
+        return(-1);
+    }
+    int d = arr[top2];
+    arr[top2++] = -1;
+    return(d);
+}
