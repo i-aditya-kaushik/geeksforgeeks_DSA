9ca84e4690c7d5ef15b24a742e25b46e42dd2ba4
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2020-05-22 14:52:22 +0530

    stock_span

---
+++ Stacks/stock_span.cpp
@@ -0,0 +1,12 @@
+void calculateSpan(int price[], int n, int S[])
+{
+    stack<int> st; 
+    st.push(0); 
+    S[0] = 1; 
+    for (int i = 1; i < n; i++) { 
+        while (!st.empty() && price[st.top()] <= price[i]) 
+            st.pop(); 
+        S[i] = (st.empty()) ? (i + 1) : (i - st.top()); 
+        st.push(i); 
+    } 
+}
