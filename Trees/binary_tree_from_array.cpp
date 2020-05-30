7cd2d3a796ced660c7eb68155cf22e165ed72144
Aditya Kaushik <i.aditya.kaushik99@gmail.com>
2020-05-31 02:27:45 +0530

    binary tree from parent array

---
+++ Trees/binary_tree_from_array.cpp
@@ -0,0 +1,29 @@
+Node *createTree(int parent[], int n)
+{
+    unordered_map<int,Node*> map;
+    for(int i=0;i<n;i++)
+    {
+        Node * newnode = new Node(i);
+        map[i] = newnode;
+    }
+    Node* root;
+    for(int i=0;i<n;i++)
+    {
+        if(parent[i]==-1)
+        {
+            root = map[i];
+            break;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(parent[i]!=-1)
+        {
+            Node* temp = map[parent[i]];
+            if(!temp->left)
+            temp->left = map[i];
+            else temp->right = map[i];
+        }
+    }
+    return root;
+}
